ccde81e1ef31fb8a772ede6b789d0990a65c27b0
srishtiYadav06 <121782388+srishtiYadav06@users.noreply.github.com>
2023-01-04 14:15:06 +0530

    Create day3_2.c

---
+++ day3_2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int sum(int n);
+int main(void) {
+  int n;
+  printf("Enter the no. of terms:\n");
+  scanf("%d", &n);
+  printf("Sum of the first %d terms of the series is %d", n, sum(n));
+  return 0;
+}
+int sum(int n) {
+  int i, j;
+  int sum = 0;
+  for (i = 1; i <= n; i++) {
+    int fac = 1;
+    for (j = 1; j <= i; j++) {
+      fac = fac * j;
+    }
+    sum = sum + fac / i;
+  }
+  return sum;
+}
+Footer
+© 2023 GitHub, 
